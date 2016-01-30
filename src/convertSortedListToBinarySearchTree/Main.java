2d972ac180f212f66eca583ce9afda2ed377cf00
grebwerd <grebwerd@gmail.com>
2016-01-29 23:41:42 -0700

    Convert a sorted linked list to a balanced binary search tree.
    Made progress it appears to work but is too slow for leet code.

--- src/convertSortedListToBinarySearchTree/Main.java
+++ src/convertSortedListToBinarySearchTree/Main.java
@@ -5,16 +5,19 @@ import isLinkedListPalidrome.ListNode;
 
 public class Main {
 
+	
+	
 	public static void main(String[] args) {
 		ListNode head = new ListNode(1);
 		ListNode second = new ListNode(2);
 		ListNode third = new ListNode(3);
 		ListNode fourth = new ListNode(4);
-		//ListNode fifth = new ListNode(5);
+		ListNode fifth = new ListNode(5);
 		
 		head.next = second;
 		second.next = third;
 		third.next = fourth;
+		fourth.next = fifth;
 		
 		Main main = new Main();
 		TreeNode retval = main.sortedListToBST(head);
@@ -23,70 +26,90 @@ public class Main {
 
 	}
 	
+	/*1. If the LinkedList is null return
+    2. Find the length of the LinkedList
+    3. Find the Mid value of the LinkedList, (n/2) if the list is even else (n/2) + 1
+    4. Find the mid Node of the linkedList
+    5. insert the mid node into the BST
+    6. recurse on the linked list 1..(mid-1) and (mid+1)..length of the linked list
+    */
 	
-	  public TreeNode sortedListToBST(ListNode head) {
-	   
-		  if(head == null || head.next == null){
-			  return null;
-		  }
-		  
-		  int mid = findMid(head);
-		  System.out.println("The value of mid is " + mid);
-		  if( mid < 2){
-			  return null;
-		  }
-		  
-		  System.out.println("the mid value is " + mid);
-		  
-		  ListNode midNode = getMidNode(head, mid);
-		  System.out.println("The value of the midNode is " + midNode.val);
-		  
-		  
-		  ListNode reverseNode = reverse(head, mid-1);
-		  System.out.println("The value of reverseNode is " + reverseNode.val);
-		  
-		  
-		  TreeNode treeHead = new TreeNode(midNode.val);
-          System.out.println("The value of the root of the tree is " + treeHead.val);
-		  
-		  
-		  constructBST(treeHead, reverseNode, midNode.next);
-
-		  return treeHead;
-	  }
+	
+	public TreeNode sortedListToBST(ListNode head){
+		TreeNode bstHead = null;
+    	if(null == head ){
+    		return null;
+    	}
+    	else if( null == head.next){
+    		return bstHead = new TreeNode(head.val);
+    	}
+    	
+    	int end = getListLength(head);
+    	
+    	bstHead = new TreeNode(-1);
+    	constructBST(bstHead, head, 1, end);
+    	
+		return bstHead;
+	}
+	
+	
+	private int getListLength(ListNode head){
+		int retval = 0;
+		while(null != head){
+			retval++;
+			head = head.next;
+		}
+		System.out.println("The list length is " + retval);
+		return retval;
+	}
+	
+	private void constructBST(TreeNode bstHead, ListNode listHead, int startIndex, int endIndex){
+		
+		System.out.println("The value of start is " + startIndex + " and the value of end is " + endIndex);
+		
+		if( startIndex > endIndex){
+			System.out.println("returning from construct BST");
+			return;
+		}
+		
+		int mid;
+		if( startIndex == endIndex){
+			mid = startIndex;
+		}else{
+		
+		 mid = ((startIndex+endIndex)/2) %2 == 0?((startIndex+endIndex)/2):((startIndex+endIndex)/2)+1;
+		System.out.println("The mid index of the linked list is " + mid);
+		}
+		int midNodeVal = getMidNode(listHead, mid);
+		System.out.println("The value of the midNodeVal is " + midNodeVal);
+		
+		insert(bstHead, midNodeVal);
+		
+		constructBST(bstHead, listHead, 1, mid-1);
+		constructBST(bstHead, listHead, mid+1, endIndex);
+	}
+	 
 	  
-	  public int findMid(ListNode head){
-		  
-		  int retval = 0;
-		  while(null != head){
-			  retval++;
-			  head = head.next;
-		  }
-		  
-		  System.out.println("listsize is " + retval);
-		  
-		  if(retval % 2 == 0){
-			  return retval/2;
-		  }
-		  else{
-			  return ((retval/2)+1);
-		  }
-		  
-	  }
-	  
-	  public ListNode getMidNode(ListNode head, int midPoint){
+	  public int getMidNode(ListNode head, int midPoint){
 		  int counter = 1;
 		  while( counter < midPoint){
 			  System.out.println("The value of head is " + head.val);
 			  head = head.next;
 			  counter++;
 		  }
-		  return head;
+		  return head.val;
 	  }
 	  
 	  public void insert(TreeNode head, int val){
 		  
-		  if(null == head){
+		 
+		  
+		  if(null == head ){
+			  return;
+		  }
+		  
+		  if( head.val == -1){
+			  head.val = val;
 			  return;
 		  }
 		  
@@ -110,39 +133,7 @@ public class Main {
 		  
 	  }
 	  
-	  public ListNode reverse(ListNode head, int endPoint){
-		  int counter = 1;
-		  ListNode prev = null;
-		  
-		  
-		  
-		  while(counter <= endPoint){
-			  ListNode next = head.next;
-			  head.next = prev;
-			  prev = head;
-			  head = next;
-			  counter++;
-		  }
-		  return prev;
-	  }
-	  
-	  public void constructBST(TreeNode treeHead, ListNode leftNode, ListNode rightNode){
-		  boolean isLeft = false;
-		  while(leftNode != null || rightNode != null){
-			  if(isLeft && leftNode != null){
-				  System.out.println("inserting the left val " + leftNode.val);
-				  insert(treeHead, leftNode.val);
-				  leftNode = leftNode.next;
-				  isLeft = false;
-			  }else if(!isLeft && rightNode != null){
-				  System.out.println("inserting the right val " + rightNode.val);
-				  insert(treeHead, rightNode.val);
-				  rightNode = rightNode.next;
-				  isLeft = true;
-			  }
-		  }
-	  }
-	  
+	
 	  
 	  public void printInOrder(TreeNode treeHead){
 		  
